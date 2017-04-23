a5cdbabbd321103c5f1e6d1c2fcaf33267e3a5b7
Pepe Nunez <josenunezdiaz@gmail.com>
2017-04-24 00:53:55 +1100

    Demo page more

--- src/mapDemo.php
+++ src/mapDemo.php
@@ -2,23 +2,14 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Weather Conditions</title>
+		<?php wp_head(); ?>
 	</head>
 	<body>
 		Holita
-
-
-		<!-- inject:js -->
-  		<!-- endinject -->
+		<script type="text/javascript" src="<?= BCWC_URL; ?>/lib/leaflet.min.js"></script>
+		<script type="text/javascript" src="<?= BCWC_URL; ?>/lib/leaflet-providers.min.js"></script>
 	</body>
 </html>
-
-
-<?php 
-
-
-
-
-
-?>
